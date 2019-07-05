eba04352d62eea6d2171681e10d4fa7d77423add
alexleung008 <542936582@qq.com>
2019-07-05 16:58:28 +0800

    CircleGame base version,It's just a base archtecture.

---
+++ CircleGame/src/main/java/com/example/CircleGame/CircleGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.CircleGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleGameApplication.class, args);
+	}
+
+}
